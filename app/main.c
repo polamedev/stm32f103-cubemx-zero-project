825791c3eb97d8e8719e6c7de8c24e1b080d5fce
polame <polame.dev@gmail.com>
2025-07-06 01:38:01 +0300

    Добавить тестовый блинк

--- app/main.c
+++ app/main.c
@@ -1,11 +1,12 @@
-#include <st_hal.h>
 #include <cubemx.h>
+#include <st_hal.h>
 
 int main()
 {
     MX_Init();
 
     while (1) {
-
+        HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+        HAL_Delay(300);
     }
 }
