0eecfeaa827afb9ace86a07456ea6d18ec54ec4a
polame <polame.dev@gmail.com>
2025-07-06 15:28:20 +0300

    Настройка директорий расположений библиотек

--- cubemx/cmake/targets/cmsis.cmake
+++ cubemx/cmake/targets/cmsis.cmake
@@ -1,14 +1,18 @@
 # --------------------------------------------------------------------------------------------------
 # Создание библиотеки cmsis
 
+if(NOT DEFINED CMSIS_DIR)
+    set(CMSIS_DIR ${CMAKE_CURRENT_SOURCE_DIR})
+endif()
+
 # На данный строчке создается цель библиотеки (название), но в ней нет еще файлов
 # Ключевое слово INTERFACE, говорит что это библиотека интерфейсов, 
 # которая содержит только header файлы и макроопределения, и их не нужно сбирать
 add_library(cmsis INTERFACE)
 
 set(CMSIS_Include_Dirs
-    ${cubemx_gen_dir}/Drivers/CMSIS/Device/ST/STM32F1xx/Include
-    ${cubemx_gen_dir}/Drivers/CMSIS/Include
+    ${CMSIS_DIR}/Device/ST/STM32F1xx/Include
+    ${CMSIS_DIR}/Include
 )
 
 # Добавляем пути включений и макроопоределения в библиотеку
--- cubemx/cmake/targets/st_hal_config.cmake
+++ cubemx/cmake/targets/st_hal_config.cmake
@@ -1,7 +1,11 @@
 # Пользовательская библиотека для конфигурации st HAL, содержит только путь включения до файла
 # с настройками stm32f1xx_hal_conf.h. Такой способ сделан по аналоги с freertos
 
+if(NOT DEFINED ST_HAL_CONFIG_DIR)
+    set(ST_HAL_CONFIG_DIR ${CMAKE_CURRENT_SOURCE_DIR})
+endif()
+
 add_library(st_hal_config INTERFACE)
 
-target_include_directories(st_hal_config INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/cubemx_hal_config)
+target_include_directories(st_hal_config INTERFACE ${ST_HAL_CONFIG_DIR})
 
