6e32430d290d29f23dd8ec6ddec8e2c0b8410a1b
polame <polame.dev@gmail.com>
2025-07-13 12:47:05 +0300

    Установка списка исходников st_hal

--- lib/lame/lame_cmakelib/target/include_st_hal.cmake
+++ lib/lame/lame_cmakelib/target/include_st_hal.cmake
@@ -1,10 +1,25 @@
 # Создание библиотеки hal
 
-option(USE_CUBEMX_ST_HAL_SRC_LIST "Использовать требуемый список файлов библиотеки, сгенерированный CubeMX" OFF)
 set(ST_MCU_NAME STM32F103xB CACHE STRING "Тип процесса в HAL")
 
 # Установка пути до исходников, нужно чтобы можно было задать любое расположение с помощью переменных
 set(ST_HAL_DIR ${CMAKE_CURRENT_SOURCE_DIR} CACHE PATH "Путь до исходников ST_HAL")
+
+option(ST_HAL_USE_FULL_SOURCES "Использовать полный список файлов для сборки" ON)
+
+# Функция по установки файлов исходников учитывая ST_HAL_DIR
+function(st_hall_sources file_list)
+
+    list(TRANSFORM file_list PREPEND "${ST_HAL_DIR}/Src/")
+    set(HAL_Src
+        ${HAL_Src}
+        ${file_list}
+    )
+
+    target_sources(st_hal PRIVATE ${HAL_Src})
+
+endfunction()
+
 
 # Создание цели библиотеки
 # Ключевое слово STATIC - говорит о создание статической библиотеке
@@ -21,20 +36,8 @@ set(HAL_Include_Dirs
     ${ST_HAL_DIR}/Inc
 )
 
-if(USE_CUBEMX_ST_HAL_SRC_LIST)
-
-    cubemx_get_st_hal_files(cubemx_hal_file_list)
-    list(LENGTH cubemx_hal_file_list cubemx_hal_file_list_size)
-    message(STATUS "st_hal_files[${cubemx_hal_file_list_size}]: " "${cubemx_hal_file_list}")
-
-    # Дополним полученный список файлов действительными путями до них
-    list(TRANSFORM cubemx_hal_file_list PREPEND "${ST_HAL_DIR}/Src/")
-    set(HAL_Src
-        ${cubemx_hal_file_list}
-    )
-
-else()
-    message(SEND_ERROR "USE_CUBEMX_ST_HAL_SRC_LIST ON - пока не поддерживается")
+if(ST_HAL_USE_FULL_SOURCES)
+    message(SEND_ERROR "ST_HAL_USE_FULL_SOURCES - пока не поддерживается")
 endif()
 
 target_compile_definitions(st_hal PUBLIC ${HAL_Defines_Syms})
