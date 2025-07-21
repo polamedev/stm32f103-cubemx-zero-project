e8547a04214f3c397109197c0a8fa8b73452c51d
polame <polame.dev@gmail.com>
2025-07-21 09:58:36 +0300

    Разделение на дескриптор и объект

--- lib/lame/soft-lib/src/SoftTimer.c
+++ lib/lame/soft-lib/src/SoftTimer.c
@@ -1,7 +1,7 @@
 #include "SoftTimer.h"
 #include <lame/SoftTimer.h>
 
-bool LAME_SoftTimer_Occur(LAME_SoftTimer *timer)
+bool LAME_SoftTimer_Occur(LAME_SoftTimer timer)
 {
     if (timer->state == LAME_SoftTimer_StateStop) {
         return false;
@@ -27,39 +27,39 @@ bool LAME_SoftTimer_Occur(LAME_SoftTimer *timer)
     return false;
 }
 
-void LAME_SoftTimer_Init(LAME_SoftTimer *timer, LAME_mSec period, LAME_SoftTimer_Mode mode)
+void LAME_SoftTimer_Init(LAME_SoftTimer timer, LAME_mSec period, LAME_SoftTimer_Mode mode)
 {
     timer->period = period;
     timer->mode   = mode;
     timer->state  = LAME_SoftTimer_StateStop;
 }
 
-LAME_SoftTimer_Mode LAME_SoftTimer_GetMode(const LAME_SoftTimer *timer)
+LAME_SoftTimer_Mode LAME_SoftTimer_GetMode(const LAME_SoftTimer timer)
 {
     return timer->mode;
 }
 
-LAME_SoftTimer_State LAME_SoftTimer_GetState(const LAME_SoftTimer *timer)
+LAME_SoftTimer_State LAME_SoftTimer_GetState(const LAME_SoftTimer timer)
 {
     return timer->state;
 }
 
-void LAME_SoftTimer_Start(LAME_SoftTimer *timer)
+void LAME_SoftTimer_Start(LAME_SoftTimer timer)
 {
     timer->state = LAME_SoftTimer_StateRun;
 }
 
-void LAME_SoftTimer_Stop(LAME_SoftTimer *timer)
+void LAME_SoftTimer_Stop(LAME_SoftTimer timer)
 {
     timer->state = LAME_SoftTimer_StateStop;
 }
 
-void LAME_SoftTimer_SetPeriod(LAME_SoftTimer *timer, LAME_mSec period)
+void LAME_SoftTimer_SetPeriod(LAME_SoftTimer timer, LAME_mSec period)
 {
     timer->period = period;
 }
 
-LAME_mSec LAME_SoftTimer_GetPeriod(const LAME_SoftTimer *timer)
+LAME_mSec LAME_SoftTimer_GetPeriod(const LAME_SoftTimer timer)
 {
     return timer->period;
 }
