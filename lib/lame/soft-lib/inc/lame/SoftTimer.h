862ad9eb3ae93c15d89c3c6bbe41eccdc56e9e6b
polame <polame.dev@gmail.com>
2025-07-21 09:35:17 +0300

    Добавить SoftTimer

--- lib/lame/soft-lib/inc/lame/SoftTimer.h
+++ lib/lame/soft-lib/inc/lame/SoftTimer.h
@@ -5,17 +5,31 @@
 #include <stdbool.h>
 
 typedef enum {
-    LAME_SoftTimer_ModeStop,
     LAME_SoftTimer_ModePeriodic,
     LAME_SoftTimer_ModeElapsed,
 } LAME_SoftTimer_Mode;
 
+typedef enum {
+    LAME_SoftTimer_StateStop,
+    LAME_SoftTimer_StateRun,
+    LAME_SoftTimer_StateElapsedComplete,
+} LAME_SoftTimer_State;
+
 typedef struct {
-    LAME_mSec           startTime; // Начальное время старта
-    LAME_mSec           period;    //< Период истечения таймера
-    LAME_SoftTimer_Mode mode;
+    LAME_mSec            startTime; // Начальное время старта
+    LAME_mSec            period;    // Период истечения таймера
+    LAME_SoftTimer_Mode  mode;
+    LAME_SoftTimer_State state;
 } LAME_SoftTimer;
 
-bool LAME_SoftTimer_Occur(LAME_SoftTimer *timer);
-void LAME_SoftTimer_Start(LAME_SoftTimer *timer, LAME_mSec period, LAME_SoftTimer_Mode mode);
+bool                LAME_SoftTimer_Occur(LAME_SoftTimer *timer);
+void                LAME_SoftTimer_Init(LAME_SoftTimer *timer, LAME_mSec period, LAME_SoftTimer_Mode mode);
+LAME_SoftTimer_Mode LAME_SoftTimer_GetMode(const LAME_SoftTimer *timer);
+
+LAME_SoftTimer_State LAME_SoftTimer_GetState(const LAME_SoftTimer *timer);
+void                 LAME_SoftTimer_Start(LAME_SoftTimer *timer);
+void                 LAME_SoftTimer_Stop(LAME_SoftTimer *timer);
+
+void      LAME_SoftTimer_SetPeriod(LAME_SoftTimer *timer, LAME_mSec period);
+LAME_mSec LAME_SoftTimer_GetPeriod(const LAME_SoftTimer *timer);
 
--- lib/lame/soft-lib/src/SoftTimer.c
+++ lib/lame/soft-lib/src/SoftTimer.c
@@ -1,18 +1,65 @@
+#include "SoftTimer.h"
 #include <lame/SoftTimer.h>
 
 bool LAME_SoftTimer_Occur(LAME_SoftTimer *timer)
 {
-    if (!timer->isRunning) {
+    if (timer->state == LAME_SoftTimer_StateStop) {
         return false;
+    }
+
+    if (timer->state == LAME_SoftTimer_StateElapsedComplete) {
+        return true;
     }
 
     LAME_mSec currentTime = LAME_Get_mSec();
 
-    /* Переполнение должно само устраниться */
+    /* Таймер прошел. Переполнение должно само устраниться */
     if (currentTime - timer->startTime > timer->period) {
-        timer->startTime = timer->period;
+        if (timer->mode == LAME_SoftTimer_ModePeriodic) {
+            timer->startTime = timer->period;
+        }
+        else {
+            timer->state = LAME_SoftTimer_StateElapsedComplete;
+        }
         return true;
     }
 
     return false;
+}
+
+void LAME_SoftTimer_Init(LAME_SoftTimer *timer, LAME_mSec period, LAME_SoftTimer_Mode mode)
+{
+    timer->period = period;
+    timer->mode   = mode;
+    timer->state  = LAME_SoftTimer_StateStop;
+}
+
+LAME_SoftTimer_Mode LAME_SoftTimer_GetMode(const LAME_SoftTimer *timer)
+{
+    return timer->mode;
+}
+
+LAME_SoftTimer_State LAME_SoftTimer_GetState(const LAME_SoftTimer *timer)
+{
+    return timer->state;
+}
+
+void LAME_SoftTimer_Start(LAME_SoftTimer *timer)
+{
+    timer->state = LAME_SoftTimer_StateRun;
+}
+
+void LAME_SoftTimer_Stop(LAME_SoftTimer *timer)
+{
+    timer->state = LAME_SoftTimer_StateStop;
+}
+
+void LAME_SoftTimer_SetPeriod(LAME_SoftTimer *timer, LAME_mSec period)
+{
+    timer->period = period;
+}
+
+LAME_mSec LAME_SoftTimer_GetPeriod(const LAME_SoftTimer *timer)
+{
+    return timer->period;
 }
